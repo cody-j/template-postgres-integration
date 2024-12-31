f20ab7e3f51c7da22d8bb33bb597504331663453
Cody J <codywj777@gmail.com>
2024-12-30 22:45:16 -0800

    add support for both local and docker

--- test/test/index.test.js
+++ test/test/index.test.js
@@ -5,13 +5,17 @@ const { Client } = require('pg');
 const db = new Client({
     user: 'postgres',
     password: 'postgres',
-    host: 'database', // Docker host alias
+    host: process.env.POSTGRES_HOST, // Docker host alias
     port: 5432,
     database: 'test' // matches POSTGRES_DB variable in database Dockerfile https://hub.docker.com/_/postgres
 });
 
 async function checkCount (id) {
     return await db.query(`select counter from "test"."test" where "id" = $1;`, [id]);
+}
+
+async function increment (id) {
+  return await axios.post(`api:3000/increment/${id}`);
 }
 
 describe('Database Tests', () => {
@@ -25,6 +29,7 @@ describe('Database Tests', () => {
   it('should work', async () => {
     const id = uuid();
     console.log('id: ', id);
+    // await increment(id);
     const results = await checkCount(id);
     console.log('results: ', results);
     expect(true).toEqual(true);
